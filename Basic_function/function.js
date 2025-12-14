540db300d73d5fc1a86fd6ba13c5e5cb8554c58d
amanmakwana-4 <ma5066770@gmail.com>
2025-12-14 13:15:55 +0530

    run function by passing array and object

--- Basic_function/function.js
+++ Basic_function/function.js
@@ -6,5 +6,26 @@ console.log(addTwoNumbers(7,2))
 function calculateCartPrice(...num){
     return num
 }
+function calCartPrice(val1,val2,...num){
+    return num
+}
 const returnValue = calculateCartPrice(200,300,100,400)
 console.log(returnValue)
+
+const returnValue2 = calCartPrice(200,300,400,500)
+console.log(returnValue2)
+
+const user={
+    name:'Aman',
+    price:200
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.price}`)
+}
+handleObject(user)
+
+const myNewArray=[10,20,30,40,50]
+function handleArray(getArray){
+    return getArray[2]
+}
+console.log(handleArray(myNewArray))
